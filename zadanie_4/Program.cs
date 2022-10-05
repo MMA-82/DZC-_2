2e8762d523ce986bf4794b9f08d95d09421092b1
Marat <brutto061@gmail.com>
2022-10-05 22:10:15 +0700

    Выполнили задание №4

---
+++ zadanie_4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string five = Console.ReadLine() ?? "0";
+int N = five.Length;
+if(N > 4)
+{
+    if(N < 6)
+    {
+        if(five[0] == five[4])
+        {
+            if(five[1] == five[3]) Console.WriteLine("Введенное число является палиндромом");
+            else Console.WriteLine("Введенное число не является палиндромом");
+        }
+        else Console.WriteLine("Введенное число не является палиндромом");
+    }
+    else Console.WriteLine("Введенное число не пятизначное!!!");
+}
+else Console.WriteLine("Введенное число не пятизначное!!!");        
