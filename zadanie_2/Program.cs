2a0a14f5e786493abd20c5dd77a15609e7d88b1b
Marat <brutto061@gmail.com>
2022-10-05 21:27:25 +0700

    Выполнили второе задание

---
+++ zadanie_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+if(a == b*b) Console.WriteLine("Первое число является квадратом второго числа");
+else Console.WriteLine("Первое число не является квадратом второго числа");
+
+if(b == a*a) Console.WriteLine("Второе число является квадратом первого числа");
+else Console.WriteLine("Второе число не является квадратом первого числа");
